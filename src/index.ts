511f15c202c3693f04d4c3d087bf60277ca1a998
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-12-14 10:24:38 +0600

    alkfhas

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://ticket-management-task-frontend-bd-calling.vercel.app', 
+  origin: '*', 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'] 
