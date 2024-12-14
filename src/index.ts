ef08283b83951ecc7b2c52b96bd9756ceccbe5a3
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-12-14 23:02:43 +0600

    added fronend url

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-  origin: '*', 
+  origin: 'http://localhost:5173', 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'] 
 }));
 
 app.options('*', (req: Request, res: Response) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.sendStatus(200);
