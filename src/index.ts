f53c3638b86ba4f6b31507bba51bccf4789e67e3
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-12-16 01:13:52 +0600

    remove backend links

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,12 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "*",
-    // origin: "*#https://ticket-management-task-fronend-bd-calling.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.options("*", cors());
 app.use(express.json());
 app.use(cors());
 
